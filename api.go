7917c868d612caa7eb8151c391e5c4059592cb0d
ezlosswm <ezlosswm@gmail.com>
2023-01-11 14:51:56 -0600

    added new route

--- api.go
+++ api.go
@@ -3,13 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-var listOfTodo TodoList
 
 type APIServer struct {
 	listenAddr string
@@ -27,43 +24,11 @@ func (s *APIServer) Run() {
 
 	// handler functions
 	mux.HandleFunc("/todo", handleTodo)
+	mux.HandleFunc("/todo/{id}", GetTodoByID)
 
 	//logs & runs server
 	fmt.Printf("New API Server running on port %s\n", s.listenAddr)
 	http.ListenAndServe(s.listenAddr, mux)
-}
-
-func handleTodo(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		GetTodo(w, r)
-	case "POST":
-		PostTodo(w, r)
-	}
-
-}
-
-// GET methods
-func GetTodo(w http.ResponseWriter, r *http.Request) {
-
-	WriteJson(w, http.StatusOK, listOfTodo)
-}
-
-func GetTodoByID(w http.ResponseWriter, r *http.Request) {
-
-}
-
-// POST methods
-func PostTodo(w http.ResponseWriter, r *http.Request) {
-	item := new(TodoItem)
-
-	if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
-		log.Fatal(err)
-	}
-
-	listOfTodo.AddItem(item.Activity)
-
-	WriteJson(w, http.StatusOK, listOfTodo)
 }
 
 func WriteJson(w http.ResponseWriter, status int, v any) error {
