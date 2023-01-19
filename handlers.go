b7b1c7ed935d3be5daa05996270c2b8cccf5e696
ezlosswm <ezlosswm@gmail.com>
2023-01-18 20:09:11 -0600

    deleted method added & refactor getbyid

--- handlers.go
+++ handlers.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func handleTodo(w http.ResponseWriter, r *http.Request) {
+func HandleTodo(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		GetTodo(w, r)
@@ -18,18 +18,7 @@ func handleTodo(w http.ResponseWriter, r *http.Request) {
 // GET methods
 func GetTodo(w http.ResponseWriter, r *http.Request) {
 
-	WriteJson(w, http.StatusOK, listOfTodo)
-}
-
-func GetTodoByID(w http.ResponseWriter, r *http.Request) {
-	// id := new(TodoItem)
-
-	item, err := listOfTodo.GetItemByID(r)
-	if err != nil {
-		log.Println(err)
-	}
-
-	WriteJson(w, http.StatusOK, item)
+	WriteJson(w, http.StatusOK, ListOfTodo)
 }
 
 // POST methods
@@ -40,7 +29,30 @@ func PostTodo(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	listOfTodo.AddItem(item.Activity)
+	ListOfTodo.AddItem(item.Activity)
 
-	WriteJson(w, http.StatusOK, listOfTodo)
+	WriteJson(w, http.StatusOK, ListOfTodo)
+}
+
+func GetTodoByID(w http.ResponseWriter, r *http.Request) {
+	// saves the id as a local variable
+	id, err := GetID(r)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if r.Method == "GET" {
+		item, err := ListOfTodo.GetItemByID(id, ListOfTodo)
+		if err != nil {
+			log.Println(err)
+
+		}
+		WriteJson(w, http.StatusOK, item)
+	}
+
+	if r.Method == "DELETE" {
+		if err := ListOfTodo.DeleteItem(id, ListOfTodo); err != nil {
+			log.Println(err)
+		}
+	}
 }
