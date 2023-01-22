379c5d32e86a83a0ae7f9ea772162c67db153dec
ezlosswm <ezlosswm@gmail.com>
2023-01-22 15:05:11 -0600

    completed field for todo

--- types.go
+++ types.go
@@ -8,6 +8,7 @@ type TodoItem struct {
 	ID          int       `json:"id"`
 	Activity    string    `json:"activity"`
 	CompletedAt time.Time `json:"completed_at"`
+	Completed   bool      `json:"completed"`
 }
 
 type TodoList struct {
